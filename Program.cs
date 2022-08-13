250d7a0f3f301f7419963638eec2ed7169d8740f
Artur <89503357000@ya.ru>
2022-08-13 21:17:20 +0600

    main

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿int x = ReadInt("Введите координату X: ");
+int y = ReadInt("Введите координату Y: ");
+
+if(x == 0 || y == 0)
+{
+Console.WriteLine("Четверть определить невозможно");
+return;
+}
+
+if (x > 0 && y > 0)
+{
+Console.WriteLine("Четверть 1");
+}
+else if(x < 0 && y > 0)
+{
+Console.WriteLine("Четверть 2");
+}
+else if(x < 0 && y < 0)
+{
+Console.WriteLine("Четверть 3");
+}
+else if(x > 0 && y < 0)
+{
+Console.WriteLine("Четверть 4");
+}
+
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
